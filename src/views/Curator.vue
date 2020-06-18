b415d39da5ad06a90b14615cdbfc07c899ea2c45
Duarte Ferreira Dias <dudias@microsoft.com>
2020-06-18 21:23:26 +0100

    curator working optimized, image thumbnails select

--- src/views/Curator.vue
+++ src/views/Curator.vue
@@ -4,9 +4,15 @@
   <!-- Tinder slider -->
   <div class="mid-center background">
     <div class="stack-wrapper">
-      <span v-if="productList.length == 0" style="text-align: center; vertical-align: middle">Curator database is empty</span>
+      <!-- Loading icon -->
+      <div v-if="!dataLoaded" style="display: flex; width:100%; height:100%">
+        <img src="https://i.ya-webdesign.com/images/loading-png-gif.gif" alt="" style="width: 100px; height: 100px; margin: auto">
+        <span v-if="productList.length == 0 && dataLoaded" style="width: 100px; height: 100px; margin: auto">Curator database is empty</span>
+      </div>
+      
       <stack ref="stack" :pages="productList" :stackinit="stackinit"></stack>
     </div>
+
     <div class="controls">
       <button @click="prev" class="button"><i class="prev"></i><span class="text-hidden">prev</span></button>
       <button @click="changeInfo " class="button"><i class="info"></i><span class="text-hidden">info</span></button>
@@ -27,15 +33,6 @@
                 <v-text-field
                     label="Title"
                     v-model="currentProduct.title"
-                ></v-text-field>
-            </v-col>
-        </v-row>
-
-        <v-row>
-            <v-col cols="12" sm="12" md="12">
-                <v-text-field
-                    label="Image URL (include 'http://'!!)"
-                    v-model="currentProduct.imageURL"
                 ></v-text-field>
             </v-col>
         </v-row>
@@ -65,7 +62,7 @@
         </v-row>
 
         <v-row>
-            <v-col cols="12" sm="6" md="3">
+            <v-col cols="12" sm="5" md="3">
                 <v-text-field
                     label="Category"
                     v-model="currentProduct.category"
@@ -73,7 +70,22 @@
             </v-col>
         </v-row>
 
-        <v-btn @click="submit()">Edit item</v-btn>
+        <v-row>
+            <v-col cols="12" sm="6" md="3" v-for="(thumbnail, index) in currentProduct.imageUrls" v-bind:key="index" class="thumbnail-image-container">
+              <img :src="thumbnail" alt="img" class="thumbnail-image" @click="updateImage(thumbnail)">
+            </v-col>
+        </v-row>
+
+        <!--<v-row>
+            <v-col cols="12" sm="12" md="12">
+                <v-text-field
+                    label="Image URL (include 'http://'!!)"
+                    v-model="currentProduct.imageURL"
+                ></v-text-field>
+            </v-col>
+        </v-row>-->
+
+        <!-- <v-btn @click="submit()">Edit item</v-btn> -->
 
     </v-container>
 
@@ -96,7 +108,8 @@ export default {
       },
       currentProduct: {},
       showInfo: false,
-      updateCurrentProductDetails: 0
+      updateCurrentProductDetails: 0,
+      dataLoaded: false
     }
   },
   mounted () {
@@ -123,6 +136,9 @@ export default {
       if(this.curatorAuxArray.length > 0) {
         this.currentProduct = this.curatorAuxArray[0]
       }
+      // Trim title and description
+      this.currentProduct.title = this.currentProduct.title.substring(0, 30)
+      this.currentProduct.description = this.currentProduct.description.substring(0, 100)
       // Update product details form
       this.updateCurrentProductDetails++
     },
@@ -135,10 +151,20 @@ export default {
       this.updateCurrentProductDetails++
     },
 
+    updateImage(imageSrc) {
+      this.currentProduct.imageURL = imageSrc
+    },
+
     async getNextProducts() {
       this.productList = await api().get('/curator/')
       this.productList = this.productList.data
       this.curatorAuxArray = this.productList.concat()
+      // Update current product
+      if(this.curatorAuxArray.length > 0) {
+        this.currentProduct = this.curatorAuxArray[0]
+      }
+
+      this.dataLoaded = true
     },
 
     async deleteCuratorItem(itemId) {
@@ -186,12 +212,13 @@ export default {
   .stack-wrapper{
     margin: 0 auto;
     position: relative;
-    z-index: 1000;
+    /*z-index: 1000;*/
     width: 420px;
-    height: 470px;
+    height: 500px;
     padding: 0;
     list-style: none;
     pointer-events: none;
+    padding-top: 30px;
   }
 
   .controls{
@@ -213,7 +240,7 @@ export default {
     font-size: 16px;
     width: 50px;
     height: 50px;
-    z-index: 100;
+    /*z-index: 100;*/
     -webkit-tap-highlight-color: rgba(0,0,0,0);
     border-radius: 50%;
     background: #fff;
@@ -274,5 +301,13 @@ export default {
     height: 0;
     color: transparent;
     display: block;
-}
+  }
+
+  .thumbnail-image {
+    width: 150px;
+  }
+
+  .thumbnail-image-container {
+    /*empty*/
+  }
 </style>
