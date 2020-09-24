535095f0cc36da77cf22d87d27638894892e2fe6
Duarte Ferreira Dias <dudias@microsoft.com>
2020-09-24 01:16:12 +0100

    improved infinite scroll, manage page fix

---
+++ plugins/infiniteloading.js
@@ -0,0 +1,7 @@
+//source:
+//https://medium.com/@sabbir.study/infinite-scroll-in-nuxt-js-f34ee86906cb
+
+import Vue from 'vue'
+import InfiniteLoading from 'vue-infinite-loading'
+
+Vue.component('infinite-loading', InfiniteLoading)
